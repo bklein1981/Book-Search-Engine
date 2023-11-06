d80b3b8e692e34b4d09e9fdc0965d8af7e1a01b8
Brian Klein <bklein1981@gmail.com>
2023-11-06 17:27:04 -0600

    implemented typeDefs, updated auth.js, started resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,44 @@
 const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, { username }) => {
+      if (!username) {
+        throw new AuthenticationError("Cannot find a user with this username")
+      }
+      const foundUser = await User.findOne({ username: username });
+      return foundUser
+    }
+  },
+
+  Mutation: {
+    //New user
+    addUser: async (parent, { username, email, password }) => {
+      const newUser = await User.create({ username, email, password })
+      const token = signToken(newUser)
+      return { token, newUser };
+    },
+
+    // login
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email })
+      if (!user) {
+        throw new AuthenticationError("Email not found")
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Password not found");
+      }
+      const token = signToken(user)
+      return { token, user }
+    },
+
+
+
+    // saveBook
+    // deleteBook
+  }
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,14 +14,32 @@ type User {
   email: String!
   password: String!
   savedBooks:[Book]
+  bookCount: Int
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+input BookInput {
+  authors:[String]
+  description: String!
+  bookId: String!
+  image: String!
+  link: String!
+  title: String!
 }
 
 type Query {
-
+me(username: String!): User
 }
 
 type Mutation {
-  
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String! password: String!): Auth
+  saveBook(username: String!, newBook: BookInput!): User
+  removeBook(bookId: String!): User
 }
 `;
 
